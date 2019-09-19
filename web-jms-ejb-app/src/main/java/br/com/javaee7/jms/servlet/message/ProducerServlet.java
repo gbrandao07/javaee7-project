8cc5a5b647f94a5ec55ae521320ec8831e7b851b
Gustavo Brandao <gustavobrandao07@hotmail.com>
2019-09-19 18:15:00 -0300

    JMS com nova API (JMSCOntext)

--- web-jms-ejb-app/src/main/java/br/com/javaee7/jms/servlet/message/ProducerServlet.java
+++ web-jms-ejb-app/src/main/java/br/com/javaee7/jms/servlet/message/ProducerServlet.java
@@ -3,12 +3,10 @@ package br.com.javaee7.jms.servlet.message;
 import java.io.IOException;
 
 import javax.annotation.Resource;
-import javax.jms.Connection;
-import javax.jms.ConnectionFactory;
-import javax.jms.MessageProducer;
-import javax.jms.Queue;
-import javax.jms.Session;
-import javax.jms.TextMessage;
+import javax.inject.Inject;
+import javax.jms.Destination;
+import javax.jms.JMSConnectionFactory;
+import javax.jms.JMSContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -25,12 +23,13 @@ import javax.servlet.http.HttpServletResponse;
 public class ProducerServlet extends HttpServlet {
 	
 	private static final long serialVersionUID = 1L;
-
-	@Resource(lookup="java:/myJmsTest/MyConnectionFactory")
-	private ConnectionFactory cf;
+	
+	@Inject
+	@JMSConnectionFactory("java:/myJmsTest/MyConnectionFactory")
+	private JMSContext jmsContext;
 	
 	@Resource(lookup="java:/queue/myQueue")
-	private Queue queue;
+	private Destination queue;
 	
 	/*
 	 * (non-Javadoc)
@@ -38,25 +37,12 @@ public class ProducerServlet extends HttpServlet {
 	 * @see javax.servlet.http.HttpServlet#doGet(javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+
 		String text = request.getParameter("message") != null ? request.getParameter("message") : "Empty message";
 		
-		try {
-			
-			Connection conn = cf.createConnection();
-			
-			Session session = conn.createSession(false, Session.AUTO_ACKNOWLEDGE);
-
-			MessageProducer publisher = session.createProducer(queue);
-			
-			conn.start();
-			
-			TextMessage message = session.createTextMessage(text);
-			
-			publisher.send(message);
-			
+		try {	
+			jmsContext.createProducer().send(queue, text);
 			response.getWriter().append("Mensagem " + text + " enviada a fila... verifique o log do console...");
-			
 		} catch (Exception e) {
 			e.printStackTrace();
 			response.getWriter().append("Erro ao enviar mensagem... verifique o log do console !!!");
